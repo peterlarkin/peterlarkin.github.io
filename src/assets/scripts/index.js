2042211ca55ee7ff9001e3b637e4f9e2d6630912
Peter Larkin <prlarkin@me.com>
2021-03-21 19:01:44 +0000

    Add about overlay

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,9 +3,11 @@ import ajax from './modules/ajax';
 import load from './modules/load';
 import sphere from './modules/sphere';
 import animations from './modules/animations';
+import about from './modules/about';
 
 load();
 ajax();
 // height();
 sphere();
 animations();
+about();
---
+++ src/assets/scripts/modules/about.js
@@ -0,0 +1,55 @@
+import gsap from 'gsap';
+
+const about = document.querySelector('#js-about');
+
+const aboutAnimation = gsap.timeline({
+  paused: true,
+  onReverseComplete: hideAbout
+})
+  .fromTo('.about__inner',
+    { x: '-100%' },
+    { x: 0, duration: 0.5, ease: 'circ.out' }
+  )
+  .fromTo('.about__content div',
+    { opacity: 0, y: 20 },
+    { opacity: 1, y: 0, duration: 0.2, stagger: 0.15, ease: 'circ.inOut' }
+  );
+
+function openAbout () {
+  document.body.classList.add('modal-open');
+  about.classList.remove('is-hidden');
+  aboutAnimation.play();
+}
+
+function closeAbout () {
+  aboutAnimation.timeScale(2).reverse();
+}
+
+function hideAbout () {
+  document.body.classList.remove('modal-open');
+  about.classList.add('is-hidden');
+}
+
+function handleClick (event) {
+  const open = event.target.closest('#js-about-open');
+  const close = event.target.closest('#js-about-close');
+
+  if (open) {
+    openAbout();
+  }
+
+  if (close) {
+    closeAbout();
+  }
+}
+
+function handleKeydown (event) {
+  if (event.key === 'Escape') {
+    closeAbout();
+  }
+}
+
+export default function () {
+  document.addEventListener('click', handleClick);
+  document.addEventListener('keydown', handleKeydown);
+}
