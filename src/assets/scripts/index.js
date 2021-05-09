438d10b62278928adafb8b77f46aa287d8a1deb3
Peter Larkin <prlarkin@me.com>
2021-05-09 17:48:21 +0100

    Re-enable sphere and animations

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,13 +1,13 @@
 import ajax from './modules/ajax';
 // import height from './modules/height';
 import load from './modules/load';
-// import sphere from './modules/sphere';
-// import animations from './modules/animations';
+import sphere from './modules/sphere';
+import animations from './modules/animations';
 import about from './modules/about';
 
 load();
 ajax();
 // height();
-// sphere();
-// animations();
+sphere();
+animations();
 about();
