e2a04f02567ae313805f1292792c18002a5453bf
Peter Larkin <prlarkin@me.com>
2021-05-10 21:42:21 +0100

    Hide case studies

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -2,12 +2,12 @@ import ajax from './modules/ajax';
 // import height from './modules/height';
 import load from './modules/load';
 import sphere from './modules/sphere';
-import animations from './modules/animations';
+// import animations from './modules/animations';
 import about from './modules/about';
 
 load();
 ajax();
 // height();
 sphere();
-animations();
+// animations();
 about();
