3e56f3e3d57d3d883016eee3b6ebd66a7065f45a
Peter Larkin <prlarkin@me.com>
2021-05-20 23:08:14 +0100

    Responsive layout for work entry template

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -1,9 +1,9 @@
-import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
+import Glide, { Controls, Breakpoints, Keyboard, Swipe } from '@glidejs/glide/dist/glide.modular.esm';
 
 export default function () {
   if (document.querySelectorAll('.glide').length) {
     new Glide('.glide', {
       type: 'carousel'
-    }).mount({ Controls, Breakpoints });
+    }).mount({ Controls, Breakpoints, Keyboard, Swipe });
   }
 }
